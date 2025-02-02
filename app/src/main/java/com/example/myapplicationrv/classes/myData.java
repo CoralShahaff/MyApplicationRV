649fe5cbdfd69b371fb8e41a6be9de633e865d1f
CoralShahaff <coshahaff@gmail.com>
2025-02-02 13:23:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationrv/classes/myData.java
@@ -0,0 +1,32 @@
+package com.example.myapplicationrv.classes;
+
+
+import com.example.myapplicationrv.R;
+
+public class myData {
+
+    public static String[] nameArray = {
+            "SpongeBob SquarePants", "Patrick Star", "Mr. Krabs", "Squidward Tentacles",
+            "Sandy Cheeks", "Plankton", "Gary", "Karen", "Mrs. Puff", "Pearl Krabs"
+    };
+    public static String[] descriptionArray = {
+            "A cheerful and optimistic sea sponge who lives in a pineapple under the sea.",
+            "SpongeBob's best friend, a lazy and dimwitted starfish.",
+            "The greedy owner of the Krusty Krab restaurant.",
+            "A grumpy and sarcastic squid who works as a cashier at the Krusty Krab.",
+            "A smart and athletic squirrel who lives in an air dome under the sea.",
+            "A small, evil genius who constantly plots to steal the Krabby Patty secret formula.",
+            "SpongeBob's pet snail who meows like a cat.",
+            "Plankton's computer wife who helps him with his evil plans.",
+            "SpongeBob's boating school teacher who fears driving accidents.",
+            "Mr. Krabs' teenage daughter, a cheerful and social whale."
+    };
+
+
+    public static Integer[] drawableArray = {R.drawable.spongebob, R.drawable.patrick, R.drawable.mrkrabs,
+            R.drawable.squidward, R.drawable.sandycheeks, R.drawable.plankton,
+            R.drawable.gary, R.drawable.karen, R.drawable.mrspuff,R.drawable.pearl};
+
+    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+}
